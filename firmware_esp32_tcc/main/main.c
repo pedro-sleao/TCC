993243ff54c60b3d70e79b0b7e5c42ccd082c4f9
pedro <pedroca242@gmail.com>
2025-01-27 09:10:32 -0300

    Adding the files related to SmartConfig for ESP32. The tests were made for ESPTOUCH and ESPTOUCH_V2.

--- firmware_esp32_tcc/main/main.c
+++ firmware_esp32_tcc/main/main.c
@@ -2,18 +2,19 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_mac.h"
+
+#include "smartconfig.h"
 
 #define LED_BUILTIN 2
 
 void app_main(void)
 {
-    gpio_reset_pin(LED_BUILTIN);
-    gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
-
-    while(1) {
-        gpio_set_level(LED_BUILTIN, 0);
-        vTaskDelay(250/portTICK_PERIOD_MS);
-        gpio_set_level(LED_BUILTIN, 1);
-        vTaskDelay(250/portTICK_PERIOD_MS);
-    }
+    ESP_ERROR_CHECK( nvs_flash_init() );
+    initialise_wifi();
 }
