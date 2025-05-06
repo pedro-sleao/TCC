60ddff05f24db0c970bc666bad4ed2f597169f3f
pedro <pedroca242@gmail.com>
2025-05-06 16:06:51 -0300

    Adding the RTC get the real time. Only made the RTC test need to change the code to use the RTC to send the data periodically.

--- firmware_esp32_tcc/components/sensors_manager/sensors_manager.c
+++ firmware_esp32_tcc/components/sensors_manager/sensors_manager.c
@@ -3,6 +3,9 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "driver/gpio.h"
+#include <time.h>
+#include <sys/time.h>
+#include "esp_sntp.h"
 
 #include "sensors_manager.h"
 #include "adc_manager.h"
@@ -20,6 +23,31 @@ static const gpio_num_t sensor_pins[] = {
 
 static const onewire_addr_t TEMPERATURE_SENSOR_ADDR = 0x5e00000000f59728;
 
+static void initialize_sntp(void)
+{
+    ESP_LOGI(TAG, "Initializing SNTP");
+    esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    esp_sntp_setservername(0, "pool.ntp.org");
+    esp_sntp_init();
+}
+
+static void obtain_time(void)
+{
+    initialize_sntp();
+
+    time_t now = 0;
+    struct tm timeinfo = { 0 };
+    int retry = 0;
+    const int retry_count = 10;
+
+    while (timeinfo.tm_year < (2024 - 1900) && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+        time(&now);
+        localtime_r(&now, &timeinfo);
+    }
+}
+
 static void enable_sensor(sensor_type_t sensor_type) {
     gpio_set_level(sensor_pins[sensor_type], 1);
 }
@@ -29,14 +57,31 @@ static void disable_sensor(sensor_type_t sensor_type) {
 }
 
 static void sensors_manager_task(void *parm) {
+    // Sensors variables
+    int turbidity;
+    float temperature;
+    // Time variables
+    time_t now;
+    struct tm timeinfo;
+    char strftime_buf[64];
+    // Buffer for mqtt messages
+    char message[32];
+
+    // Set timezone to Brazil (Recife)
+    setenv("TZ", "<-03>3", 1);
+    tzset();
+    // RTC config
+    obtain_time();
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    // Print local time
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    ESP_LOGI(TAG, "The current date/time in Recife is: %s", strftime_buf);
+
     for (int i = 0; i < sizeof(sensor_pins) / sizeof(sensor_pins[0]); i++) {
         gpio_set_direction(sensor_pins[i], GPIO_MODE_OUTPUT);
         ESP_LOGI(TAG, "GPIO %d successfully configured!", sensor_pins[i]);
     }
-
-    int turbidity;
-    float temperature;
-    char message[32]; // Buffer for mqtt messages
 
     while (1) {
         adc_init();
@@ -56,6 +101,10 @@ static void sensors_manager_task(void *parm) {
         snprintf(message, sizeof(message), "%.2f", temperature);
         mqtt_publish("sensor/temperature", message);
 
+        time(&now);
+        localtime_r(&now, &timeinfo);
+        strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+        ESP_LOGI(TAG, "The current date/time in Recife is: %s", strftime_buf);
         ESP_LOGI(TAG, "Turbidity = %d", turbidity);
         ESP_LOGI(TAG, "Temperature = %.2f", temperature);
         vTaskDelay(pdMS_TO_TICKS(10000));
@@ -66,4 +115,6 @@ void init_sensors_task(void) {
     ESP_LOGI(TAG, "Initializing sensors manager task...");
     xTaskCreate(sensors_manager_task, "sensors_manager_task", 4096, NULL, 3, NULL);
 }
+
+
 
